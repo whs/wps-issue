8323d97cc8174084810cc0a4936ca1c82e74715d
Manatsawin Hanmongkolchai <manatsawin@wongnai.com>
2018-05-30 09:12:48 +0700

    Removed babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = {
 	mode: 'development',
 	entry: {
-		a: ['babel-polyfill', path.resolve('entry/a.js')],
-		b: ['babel-polyfill', path.resolve('entry/b.js')],
-		c: ['babel-polyfill', path.resolve('entry/c.js')],
+		a: path.resolve('entry/a.js'),
+		b: path.resolve('entry/b.js'),
+		c: path.resolve('entry/c.js'),
 	},
 	output: {
 		path: path.resolve('./dist'),
